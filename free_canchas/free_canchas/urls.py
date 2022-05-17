050f0f0833dd218d68cae263a9365c561b878ade
Dieg2020 <diego.ignaceo.lb@gmail.com>
2022-05-17 13:20:13 -0400

    pagina de inicio

--- free_canchas/free_canchas/urls.py
+++ free_canchas/free_canchas/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('',include('free_canchas.urls')),
     path('admin/', admin.site.urls),
 ]
